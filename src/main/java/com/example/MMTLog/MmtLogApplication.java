d2ff9af5abde5b9e4230823508d304980d914b43
Rishabh Agarwal <rishabh.agarwal1@delhivery.com>
2021-02-21 13:27:52 +0530

    refacotred to take inputs while application is running

--- src/main/java/com/example/MMTLog/MmtLogApplication.java
+++ src/main/java/com/example/MMTLog/MmtLogApplication.java
@@ -1,6 +1,5 @@
 package com.example.MMTLog;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
@@ -18,36 +17,41 @@ public class MmtLogApplication {
 		System.out.println("Please provide the keys name can be present in JSON file");
 		String keysString = scanner.nextLine();
 
-		System.out.println("Please provide the multiple values on which single occurrence has to find");
-		String findOnSingleValueString = scanner.nextLine();
-
-		System.out.println("Please provide the input values on which multiple occurrence has to find");
-		String findOnMultipleString = scanner.nextLine();
-
 		String[] keys = keysString.split(" ");
-		String[] findOnSingleValue= findOnSingleValueString.split(" ");
-		String[] findOnMultipleAnd = findOnMultipleString.split(" and ");
-		System.out.println(findOnSingleValue.toString());
-		String[] findOnMultipleOr = findOnMultipleString.split(" or ");
-
-
 		ProcessLog processLog = new ProcessLog();
 		System.out.println("reading file path is " + filePath);
 		processLog.readLogsFromFile(filePath, keys);
 
 		CheckOccurrence checkOccurrence = new CheckOccurrence();
+		String[] arrays = null;
+		System.out.println("****** Input format ***********");
+		System.out.println("please provide the input to test the application " );
+		System.out.println("input can be in format of like");
+		System.out.println("To test on single value   eg:     " + "DESKTOP INFO");
+		System.out.println("To test on multiple values on and  eg:     " + "DESKTOP and INFO");
+		System.out.println("To test on multiple values on or    eg:   " + "DESKTOP or INFO");
+		while (true) {
+			String input = scanner.nextLine();
+			arrays = input.split(" and ");
+			if(arrays.length > 1) {
+				System.out.println("****** Output ***********");
+				System.out.println("Finding occurrence in values based on AND  is " + checkOccurrence.findOccurrenceOfValuesOnAnd(arrays));
+			} else {
+				arrays = input.split(" or ");
+				if(arrays.length > 1) {
+					System.out.println("****** Output ***********");
+					System.out.println("Finding occurrence in values based on OR  is " + checkOccurrence.findOccurrenceOfValuesOnOr(arrays));
+				} else {
+					arrays = input.split(" ");
+					for(String onSingleValue : arrays) {
+						System.out.println("****** Output ***********");
+						System.out.println("Finding occurrence for single value is for value " + onSingleValue + " "
+							+ checkOccurrence.findOccurrenceOnSingleValue(onSingleValue));
+					}
 
-		for(String onSingleValue : findOnSingleValue) {
-			System.out.println("");
-			System.out.println("Finding occurrence for single value is for value " + onSingleValue + " "  + checkOccurrence.findOccurrenceOnSingleValue(onSingleValue));
-		}
+				}
+			}
 
-		if(findOnMultipleAnd.length > 0) {
-			System.out.println("Finding occurrence in values based on AND  is " + checkOccurrence.findOccurrenceOfValuesOnAnd(findOnMultipleAnd));
-		}
-
-		if(findOnMultipleAnd.length > 0) {
-			System.out.println("Finding occurrence in values based on OR  is " + checkOccurrence.findOccurrenceOfValuesOnOr(findOnMultipleOr));
 		}
 	}
 
